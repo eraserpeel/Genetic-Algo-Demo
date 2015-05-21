fb68675ffee6631b9243afa18d14ee9d817129f7
slogmore <slogmore42@gmail.com>
2015-05-21 19:43:06 +0800

    Added a rudimentary protozoa class

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class GAWindow(pyglet.window.Window):
     def __init__(self):
         super().__init__(600, 600)
         pyglet.clock.schedule_interval(self.update, 1.0/60.0)
-        width, height = self.get_size()
+        self.window_width, self.window_height = self.get_size()
 
         self.protozoa = []
         self.batch = pyglet.graphics.Batch()
@@ -18,9 +18,9 @@ class GAWindow(pyglet.window.Window):
         img = pyglet.resource.image("imgs/protozoa-green.png")
 
         for i in range(0, 10):
-            x = 300
-            y = 300
-            self.protozoa.append(Protozoa(x, y, img, self.batch))
+            x = random() * (self.window_width - 100) + 50
+            y = random() * (self.window_height - 100) + 50
+            self.protozoa.append(Protozoa((x, y), self.get_size(), img, self.batch))
 
 
     def on_draw(self):
